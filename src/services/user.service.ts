cff101665613ac9fdefbb4f216b03b60f711c509
sashalehedza <sashalehedza@gmail.com>
2024-09-17 18:29:48 +0300

    fix

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,21 +8,6 @@ class UserService {
   }
 
   public async create(dto: Partial<IUser>): Promise<IUser> {
-    // if (!dto.name || dto.name.length < 3) {
-    //   throw new ApiError(
-    //     "Name is required and should be at least 3 characters long",
-    //     400,
-    //   );
-    // }
-    // if (!dto.email || !dto.email.includes("@")) {
-    //   throw new ApiError("Email is required and should be valid", 400);
-    // }
-    // if (!dto.password || dto.password.length < 6) {
-    //   throw new ApiError(
-    //     "Password is required and should be at least 6 characters long",
-    //     400,
-    //   );
-    // }
     return await userRepository.create(dto);
   }
 
@@ -35,21 +20,6 @@ class UserService {
   }
 
   public async updateById(userId: string, dto: IUser): Promise<IUser> {
-    // if (!dto.name || dto.name.length < 3) {
-    //   throw new ApiError(
-    //     "Name is required and should be at least 3 characters long",
-    //     400,
-    //   );
-    // }
-    // if (!dto.email || !dto.email.includes("@")) {
-    //   throw new ApiError("Email is required and should be valid", 400);
-    // }
-    // if (!dto.password || dto.password.length < 6) {
-    //   throw new ApiError(
-    //     "Password is required and should be at least 6 characters long",
-    //     400,
-    //   );
-    // }
     return await userRepository.updateById(userId, dto);
   }
 
